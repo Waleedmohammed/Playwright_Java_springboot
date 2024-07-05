b4f6edc70ec49944c3b3581775e988aea6582d61
waleed.elbarbary <n448321@pagonxt.com>
2024-07-05 19:16:50 +0200

    Pushing initial setup for Testing Framework

---
+++ src/main/java/com/qa/verivox/QaChallengeVerivoxApplication.java
@@ -0,0 +1,13 @@
+package com.qa.verivox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaChallengeVerivoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaChallengeVerivoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/verivox/QaChallengeVerivoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.verivox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaChallengeVerivoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
